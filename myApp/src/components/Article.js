6a020c8c582e1d9fd759f3f97ec64da770dae8ef
RomariosGoIT <romantgt@gmail.com>
2018-09-14 11:00:07 +0300

    commit

--- myApp/src/components/Article.js
+++ myApp/src/components/Article.js
@@ -1,36 +1,32 @@
 import React, {Component} from 'react';
 
 class Article extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            isOpen: props.defaultOpen,
-        }
+        // this.state = {
+        //     isOpen: props.defaultOpen,
+        // }
 
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return this.state.isOpen !== nextState.isOpen
+    // }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.isOpen !== nextState.isOpen
-    }
-
-    componentWillReceiveProps(nextProps) {
-        // console.log('will recive props')
-        if(nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
-            isOpen: nextProps.defaultOpen,
-        })
-    }
+    // componentWillReceiveProps(nextProps) {
+    //     // console.log('will recive props')
+    //     if(nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+    //         isOpen: nextProps.defaultOpen,
+    //     })
+    // }
     
     componentWillUpdate() {
         console.log('will update')
     }
 
     render () {
-        const {article} = this.props;
-        const body = this.state.isOpen && <section className='card-text' style={{marginTop:'10px'}}>{article.text}</section>
+        const {article, isOpen, onButtonClick} = this.props;
+        const body = isOpen && <section className='card-text' style={{marginTop:'10px'}}>{article.text}</section>
         const date = <h6 className='card-subtitle text-muted'> creation date: {(new Date(article.date)).toDateString()}</h6>
-        const buttonText = this.state.isOpen ? 'Close' : 'Open';
-        const button = <button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">{buttonText}</button>
+        const buttonText = isOpen ? 'Close' : 'Open';
+        const button = <button onClick={onButtonClick} className="btn btn-primary btn-lg float-right">{buttonText}</button>
         return (
             <div className='card mx-auto' style={{width: '80%'}}>
                 <div className='card-header'>
@@ -48,9 +44,9 @@ class Article extends Component {
     }
 
     handleClick = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
+        // this.setState({
+        //     isOpen: !this.state.isOpen
+        // })
     }
 }   
 
--- myApp/src/components/ArticleList/index.js
+++ myApp/src/components/ArticleList/index.js
@@ -3,10 +3,16 @@ import Article from '../Article'
 import './style.css'
 
 export default  class ArticleList extends PureComponent {
+    state = {
+        openArticleId: null,
+    }
     render(){
         const articleList = this.props.articles.map((art, index) => 
         <li key={art.id} className='article-list__li'>
-        <Article article = {art} defaultOpen = {index === 0} />
+        <Article article = {art} 
+            isOpen = {this.state.openArticleId === art.id}
+            onButtonClick = {this.handleClick.bind(this, art.id)} 
+        />
         </li>
         )
     return (
@@ -15,5 +21,9 @@ export default  class ArticleList extends PureComponent {
         </ul>
     )
     }
+
+    handleClick = openArticleId => this.setState({ 
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+     })
 
 }
